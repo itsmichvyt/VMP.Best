74f5cfe7ac2bfe02ff710351ce49b4b17f57c785
its_michvyt <147197452+itsmichvyt@users.noreply.github.com>
2025-05-06 20:13:45 +0100

    Update offsets.h

--- offsets.h
+++ offsets.h
@@ -1,8 +1,8 @@
-// no sdk offsets
+// no sdk offsets, for niggos0
 #pragma once
 namespace offsets {
 	inline uintptr_t
-		uworld = 0x176C0298 , // non-sdk
-		bone_array = 0x5E8 , // non-sdk
+		uworld = 0x17AA2698, // Good, ig
+		bone_array = 0x5E8 , // idk check it (06/05/25)
 		component_to_world = 0x1E0 , // non-sdk
 }
