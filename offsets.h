d444cac4e3b45734121fc4cd98a0f7948ee86f2f
its_michvyt <147197452+itsmichvyt@users.noreply.github.com>
2025-05-06 20:03:02 +0100

    Create offsets.h

---
+++ offsets.h
@@ -0,0 +1,8 @@
+// no sdk offsets
+#pragma once
+namespace offsets {
+	inline uintptr_t
+		uworld = 0x176C0298 , // non-sdk
+		bone_array = 0x5E8 , // non-sdk
+		component_to_world = 0x1E0 , // non-sdk
+}
